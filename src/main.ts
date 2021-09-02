ed4159c737d937ebd787405fd6f5c9dbb9b6e3c4
AnshumGill <anshumgill@gmail.com>
2021-09-02 20:37:05 +0530

    Smooooth scroller

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
-import OverlayScrollbars from "overlayscrollbars";
+import Scrollbar from "smooth-scrollbar";
 
 if (environment.production) {
 	enableProdMode();
@@ -13,11 +13,4 @@ platformBrowserDynamic()
 	.bootstrapModule(AppModule)
 	.catch((err) => console.error(err));
 
-OverlayScrollbars(document.body, {
-	nativeScrollbarsOverlaid: {
-		initialize: false,
-	},
-	scrollbars: {
-		autoHide: "scroll",
-	},
-});
+Scrollbar.init(document.body);
